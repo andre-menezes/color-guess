77d541bf0e08809441fc33c323eb1733e81b4844
Andre Menezes <dehhmenezes87@gmail.com>
2021-10-05 12:59:01 -0300

    implemented answer icon

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import correct from '../images/correct.svg';
+import wrong from "../images/wrong.svg";
 
 export default function Color(props) {
   const { red, green, blue, setAnswer, correctColor, disable, setDisable } = props;
@@ -32,29 +34,29 @@ export default function Color(props) {
       onClick={({ target }) => {
         if (!disable) {
           if (
-            red === correctColor.red
-            && green === correctColor.green
-            && blue === correctColor.blue
+            red === correctColor.red &&
+            green === correctColor.green &&
+            blue === correctColor.blue
           ) {
             setDisable(true);
-            target.classList.add('correct');
-            setImgAnswer('correct');
-            return setAnswer('Acertou!')
+            target.classList.add("correct");
+            setImgAnswer("correct");
+            return setAnswer("Acertou!");
           }
           setDisable(true);
-          target.classList.add('wrong');
-          setImgAnswer('wrong');
-          return setAnswer('Infelizmente você errou!')
+          target.classList.add("wrong");
+          setImgAnswer("wrong");
+          return setAnswer("Infelizmente você errou!");
         }
       }}
     >
-      {
-        imgAnswer !== ""
-        ? imgAnswer === 'correct'
-            ? <img alt='correct' />
-            : <img alt='wrong' />
-        : null            
-      }
+      {imgAnswer !== "" ? (
+        imgAnswer === "correct" ? (
+          <img src={correct} alt="correct" width="40" />
+        ) : (
+          <img src={wrong} alt="wrong" width="40" />
+        )
+      ) : null}
     </div>
   );
 }
